3b133cafe92d2009aea53db1b34b6ec925f4bcb8
GyovanneMS <gyovannecontato@gmail.com>
2024-03-22 15:23:26 -0300

    The project

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, StatusBar, TextInput, Pressable } from 'react-native';
+import Grafico from './src/components/Grafico';
+import Janela from './src/components/Janela';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [limite, definirLimite] = useState(1000);
+  const [gastos, definirGastos] = useState([]);
+  const [valor, definirValor] = useState(0);
+  const [categoria, definirCategoria] = useState("");
+
+  function AdicionarGastos() {
+    const novoGasto = {categoria, valor};
+    definirGastos([...gastos, novoGasto]);
+  }
+
+  function TotalGastos(){
+    var total = 0;
+    for (var gasto of gastos)
+      total += parseFloat(gasto.valor);
+    return total
+  }
+  return <ScrollView>
+    <StatusBar barStyle="dark-content" backgroundColor="#fff"/>
+
+    <Grafico porcentagem={(TotalGastos()/limite) * 100}/>
+
+    <Janela>
+
+      <Text style={{fontSize: 18, marginBottom: 16, textAlign: "center"}}> Definir Limite</Text>
+      <TextInput
+        style={{backgroundColor:"#fff", padding: 8}}
+        value={limite}
+        onChangeText={ definirLimite }
+        placeholder='Limite (R$)'
+        keyboardType="number-pad"
+      /> 
+      {
+        //keyboardType serve para mudar o tipo de teclado, nesse caso para numérico
+      }
+
+    </Janela>
+    <Janela>
+
+      <Text style={{fontSize: 18, marginBottom: 16, textAlign: "center"}}> Adicionar Gastos </Text>
+      <TextInput
+        style={{backgroundColor: "#fff", marginBottom: 16, padding: 8}}
+        value={categoria}
+        onChangeText={ definirCategoria }
+        placeholder='Gasto)'
+        keyboardType='default'
+      /> 
+      <TextInput
+        style={{backgroundColor: "#fff", marginBottom: 16, padding: 8}}
+        value={valor}
+        onChangeText={ definirValor }
+        placeholder='Valor (R$)'
+        keyboardType='number-pad'
+      /> 
+      <Pressable onPress={AdicionarGastos}>
+        <View style={{borderStartColor: "#698269"}}>
+          <Text style={{color: "#fff", paddingVertical: 16, textAlign: "center"}}> Adicionar </Text>
+        </View>
+      </Pressable>
+
+    </Janela>
+
+    <Janela>
+        <Text style={{ fontSize: 18, marginBottom: 16, textAlign: "center"}}>
+          Histórico de Gastos
+        </Text>
+        {
+          gastos.map(function(gasto, indice){
+            return <Text key={indice} style={{color: "#698269"}}>
+              {gasto.categoria} - R$ {gasto.valor}
+            </Text>
+          })
+        }
+    </Janela>
+
+  </ScrollView>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
